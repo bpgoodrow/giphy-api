d8f9622bc302e117c5a09797e3203733bf377754
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-27 16:44:44 -0700

    EOD commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import $ from 'jquery';
+import $ from 'jquery';
 
+$(document).ready(function () {
+  $('#searchGif').click(function () {
+    const userSearch = $('#gifSearch').val();
+    $('#gifSearch').val("");
 
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=45V33K4IKnEJwzuwTIgY3hKVOHq1sdr3&q=${userSearch}&limit=25&offset=0&rating=pg-13&lang=en`;
 
-// import { Object } from './businesslog.js';
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        /* eslint-disable */
+        getElements(response);
+        /* eslint-enable*/
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+    /*eslint-disable*/
+    function getElements(response) {
+      for (let i = 0; i < `${response.data.length}`; i++) {
+        $('.showData').append(`<img src="${response.data[i].images.original.url}">`);
+      };
+    }
+    /*eslint-enable*/
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -18,7 +19,8 @@ plugins: [
       title: 'Template',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
@@ -33,7 +35,25 @@ plugins: [
       test: /\.js$/,
       exclude: /node_modules/,
       loader: "eslint-loader"
-      }
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
     ]
   }
 };
